549151f602102e44679da1a02be1c9af3033593f
Eduardo Simões <eduardorsimoes97@gmail.com>
2022-11-29 14:04:24 -0300

    Req 25

--- app/backend/src/database/routes/matches.routs.ts
+++ app/backend/src/database/routes/matches.routs.ts
@@ -1,12 +1,13 @@
 import * as express from 'express';
 import MatchController from '../controller/matches.controller';
+import matchesMiddleware from '../middlewares/matches.middleware';
 
 const matchesRouter = express.Router();
 const controller = new MatchController();
 
 matchesRouter.get('/', controller.returnMatchesByByProgress);
 
-matchesRouter.post('/', controller.createNewMatch);
+matchesRouter.post('/', matchesMiddleware, controller.createNewMatch);
 
 matchesRouter.patch('/:id/finish', controller.updateToFinished);
 
